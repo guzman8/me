dc055d873a0aca75a6146b451f6a9bd9df92140b
Mireia Badell Romagosa <mimi@MacBook-Air-de-Mireia.local>
2022-11-27 15:51:04 +0100

    nuevos cambios

--- expServ.js
+++ expServ.js
@@ -18,3 +18,31 @@ const PORT = 8000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+/*document.getElementById("myBtn").addEventListener("click", sendMail);
+
+function sendMail(name, email, subject, message) {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.set('Authorization', 'Basic ' + base64.encode('<API Key>'+":" +'<Secret Key>'));
+
+  const data = JSON.stringify({
+    "Messages": [{
+      "From": {"Email": "<YOUR EMAIL>", "Name": "<YOUR NAME>"},
+      "To": [{"Email": email, "Name": name}],
+      "Subject": subject,
+      "TextPart": message
+    }]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: data,
+  };
+
+  fetch("https://api.mailjet.com/v3.1/send", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}*/
